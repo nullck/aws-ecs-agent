b88d65e87b81cf1c906acbb2bbbd7b13c7def120
nullck <nullck@gmail.com>
2015-10-02 18:15:56 -0300

    fixed metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          'All rights reserved'
 description      'Installs/Configures aws-ecs-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends "docker"
+depends 'docker', '~> 0.36.0'
